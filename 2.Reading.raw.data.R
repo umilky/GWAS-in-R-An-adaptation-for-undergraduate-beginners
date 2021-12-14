f9d2baa8fa115a513d55076cdadf52a25b19c815
umilky <94842043+umilky@users.noreply.github.com>
2021-12-14 08:38:12 -0800

    up

--- 2.Reading.raw.data.R
+++ 2.Reading.raw.data.R
@@ -1,20 +1,24 @@
 # === Reading Data in R ========================================================
 
+# Script 1 is required to run first to access folder paths and to input raw 
+# data required to run this script.
+
+
 # Read PLINK files to create a list
-# PLINK is a genome association analysis toolkit
-# bed files contain individual genotypes
-# bin files contain the locations of all SNPs in the data in the genome
-# fam files contain information about the individual including family pedigrees
+# PLINK is a genome association analysis toolkit.
+# bed files contain individual genotypes.
+# bin files contain the locations of all SNPs in the data in the genome.
+# fam files contain information about the individual including family pedigrees.
 
 geno.me <- read.plink(gwas.data$bed, gwas.data$bim, gwas.data$fam)
 
-# creating the genotype object from the geno.me list 
+# creating the genotype object from the geno.me matrix
 
 genotype <- geno.me$genotype 
 print(genotype)
 head(genotype)
 
-# obtain the SNP information from geno list
+# obtain the SNP information from geno.me matrix
 geno.bim <- geno.me$map
 colnames(geno.bim) <- c("chr", "SNP", "gen.dist", "position", "A1", "A2")
 print(head(geno.bim))
@@ -27,12 +31,12 @@ colnames(SNP.genome) <- c("chromosome", "SNP", "gen.dist", "position", "A1",
 
 print(head(SNP.genome))
 
-#removing raw data to create space 
-rm(geno.me)
 
-#reading phenotype data from the clinical file 
-#apparently using colClasses saves data importing time
-#Also, cannot subset genotype without rearranging using colClasses first
+# reading phenotype data from the clinical file 
+# Using colClasses saves data importing time
+# colClasses creates a vector of column classes used for tabular reading. 
+# This is required for subsetting genotype in the next step. 
+
 phenotype <- read.csv(clinical.data,
                      colClasses = c("character", "factor", "factor", 
                                     rep("numeric", 4)))
@@ -53,3 +57,4 @@ genotype.s <- genotype[phenotype$FamID, ]
 
 # on to script 3.Data.filtering 
 
+#___ end _______________________________________________________________________
