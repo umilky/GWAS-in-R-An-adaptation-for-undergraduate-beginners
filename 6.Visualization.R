c2bd5669d2ddfea7bb7a6c77ca359ab760d26f28
umilky <94842043+umilky@users.noreply.github.com>
2021-12-14 08:29:18 -0800

    Up

--- 6.Visualization.R
+++ 6.Visualization.R
@@ -1,4 +1,8 @@
 # === Visualization ============================================================
+# Scripts 1-5 are required to run first to access folder paths and obtain the 
+# data required for creating plots.
+# This file creates plots from the analysed data.
+# Plots are saved as pdfs to c.graphics folder.
 
 ## Visualizing the results of the GWAS analysis 
 
@@ -8,24 +12,34 @@
 # non-significant p-values, suggesting potentially false findings.
 # Multiple signals in the CETP region suggest that this may be a true signal.
 
+
+
+# creating Manhattan Plot and saving it as a .pdf. 
+# we use a function created by Reed et. al (2015) to generate this plot. 
+
+# loading Manhattan Plot function 
 source("Manhattan.plot.R")
+
+pdf(paste(p.graphics, "ManhattanPlot.pdf", sep = ""))
 par(mfrow = c(1,1))
-
-# creating Manhattan Plot. 
-# need to save as .pdf later
 manhattan.plot.s <- GWAS.manhattan(GWAS.comb)
-manhattan.plot.s
+dev.off()
 
-
-# creating QQ Plot 
+ 
 # Q-Q plots are used to visualize the relationship between the expected
 # and observed distributions of SNP-level test statistics. We use the function 
 # estlambda from the package GenABEL to generate our QQ plots. 
 
-#creating QQ plot. Save as pdf later
 GWAS.output$t.value <- as.numeric(GWAS.output$t.value)
-lambda.plot.s <- estlambda(GWAS.output$t.value^2, plot = TRUE, method = "median")
-lambda.plot.s
-# END? 
+
+# creating QQ Plot and saving it as a .pdf
+pdf(paste(p.graphics, "ManhattanPlot.pdf", sep = ""))
+lambda.plot.s <- estlambda(GWAS.output$t.value^2, plot = TRUE, 
+                           method = "median")
+dev.off()
+
+#___ end _______________________________________________________________________
+
+
 
 
